12ed9b47f785c5d0833496fc752d7bdf4daf4c59
sandi <vanbianasandi@gmail.com>
2024-06-01 09:03:28 +0700

    tugas-sandi

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="learnpage">learnpage</router-link>
+    
+  </nav>
+  <router-view/>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+.sec1{
+  padding-left: 500px;
+  margin-top: -25px
+}
+.sec2{
+  padding-left: 700PX;
+  margin-top: -25px;
+}
+.sec3{
+  padding-left: 890px;
+  margin-top: -25px;
+}
+.sec4{
+  padding-left: 1090px;
+  margin-top: -25px;
+}
+
+
+
+</style>
---
+++ src/views/learnpage.vue
@@ -0,0 +1,43 @@
+<template>
+<div class="learnpage"></div>
+
+<h1>learn vue and page</h1>
+<div  class="fotogrup"><img  alt="foto1" src="../assets/Group 522.png"></div>
+<div class="background"></div>
+<div class="sec1">section1</div>
+<div class="sec2">section2</div>
+<div class="sec3">section3</div>
+<div class="sec4">section4</div>
+<div class="coding"><img src="../assets/coding_foto.jpg" alt=""></div>
+<div class="background1"></div>
+<h1 class="moving1"><mark>moving sumsum</mark></h1>
+<p class="lorem1">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus labore dolore recusandae tempora, quo commodi quia modi ullam mollitia itaque, dolores consectetur ab ut, ducimus pariatur veritatis deleniti harum excepturi.</p>
+<div class="background2"></div>
+<h1 class="moving2"><mark> LOREM INSUM</mark></h1>
+<p class="lorem2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia quas in fugiat consequatur tempore, soluta cupiditate nesciunt eum officiis qui necessitatibus animi nostrum earum vero veniam sed cumque, fugit magni!</p>
+<div class="foto2"><img src="../assets/Union (1).png" alt=""></div>
+<div class="background3"></div>
+<h1 class="about">ABOUT US</h1>
+<p class="lorem3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae consequatur rem similique consequuntur, ea expedita eos repellat consectetur! In dolore laborum delectus perferendis ducimus facilis tempore impedit eveniet perspiciatis ut.</p>
+<div class="background4"></div>
+<h1 class="weare"><mark>we are family</mark></h1>
+<p class="lorem4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora accusantium, ex ipsum maxime id dolore harum animi fugit itaque corrupti eveniet laboriosam modi soluta incidunt illum ea voluptas dolores similique?</p>
+<div class="background5"></div>
+<h1 class="services"><mark>SERVICES</mark></h1>
+<div class="foto3"><img src="../assets/Rectangle 250.png" alt=""></div>
+<div class="background6"></div>
+<h1 class="lorem5"> lorem ipsum</h1>
+<p class="loremp1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas mollitia laboriosam assumenda deleniti distinctio tempore sed ad magni! Pariatur minima ut, ex quidem iste numquam cum recusandae tempora illum similique.</p>
+<div class="foto4"><img src="../assets/Rectangle 250 (1).png" alt=""></div>
+<div class="background7"></div>
+<h1 class="lorem6">Lorem ipsum</h1>
+<p class="lorem7">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, dolores incidunt, asperiores, neque nam ullam error quasi nemo obcaecati modi autem tempore exercitationem deleniti qui blanditiis voluptate tenetur aspernatur officia!</p>
+
+</template>
+
+<style lang="scss">
+
+@import '../assets/scss/index.scss';
+
+</style>
+
